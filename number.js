d73a4d5da82a024651e82c630df8a4c99ce3e34f
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2022-06-19 16:00:54 +0530

    error rectified

--- number.js
+++ number.js
@@ -1,19 +1,20 @@
 var number = document.querySelector("#Number");
-var output = document.querySelector("#converted");
+var output = document.querySelector("#Converted");
 var btn = document.querySelector("#submit");
 
 btn.addEventListener("click", clickHandler)
-console.log("inside js");
+// console.log("inside js");
 function clickHandler() {
     var input = number.value;
     var URL = "https://api.funtranslations.com/translate/numbers.json?text=" + input;
-    console.log(URL);
+    // console.log(URL);
     fetch(URL)
         .then(response => response.json())
         .then(json => {
             var translatedText = json.contents.translated;
+            // console.log("ouput:" + translatedText);
             output.innerText = translatedText;
-            console.log(translatedText);
+
         }).catch(errorHandler)
 
 };
